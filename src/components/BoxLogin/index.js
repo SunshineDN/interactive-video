5ebc3ef1c27e470f9eb2c18feaa84dd4e33539a0
Douglas Augusto <douglasaugusto1103@gmail.com>
2023-03-07 00:32:31 -0300

    Ajustado semânticas html

--- src/components/BoxLogin/index.js
+++ src/components/BoxLogin/index.js
@@ -1,38 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./style.css"
 const BoxLogin = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [cargo, setCargo] = useState("");
+
+  function handleMessage(){
+    if (email === "" || password === "" || cargo === "" ){
+      return
+    }
+    alert("Você entrou")
+  }
   return (
     <div>
-        
-        <div className='main'>
-        <h5>Usuario</h5>
-    <form action="#"> <input type="email" placeholder="Digite o email" /> 
-    <h5>Senha</h5>
-    <input type="password" placeholder="Digite a senha" /> 
-    <br/>
-    <br/><button className='bot'>Acessar</button>
-     </form>
-     </div>
-     
-
-     <br/>  
-    <div className='radio'>
-     <fieldset>
-     <input type="radio" id="Aluno" name="cargo" value="Aluno"
-             checked/>
-      <label for="aluno">Aluno</label>
-      <input type="radio" id="Professor" name="cargo" value="Professor"/    >
-      <label for="dewey">Professor</label>
-     </fieldset>
-     </div>
-        
-
-     </div>
-        
-        
-     
+      <form className='container'>
+        <div className='input-wrapper'>
+          <label htmlFor="user">Email</label>
+          <input id="user" type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="Digite o email" required />
+          <label className='passLabel' htmlFor="pass">Senha</label>
+          <input id='pass' type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Digite a senha" required />
+        </div>
+        <div className='radio-wrapper'>
+          <div className='radios'>
+            <input type="radio" id="aluno" name="cargo" value="Aluno" onChange={e => setCargo(e.target.value)} required />
+            <label htmlFor="aluno">Aluno</label>
+          </div>
+          <div className='radios'>
+            <input type="radio" id="professor" name="cargo" value="Professor" onChange={e => setCargo(e.target.value)} required />
+            <label htmlFor="professor">Professor</label>
+          </div>
+        </div>
+        <input type="submit" className='bot' value="Acessar" onClick={handleMessage} />
+      </form>
+    </div >
   )
 }
 
 export default BoxLogin
- 
